a970ec714f16f42099b27cd71a1085241f90174a
ArturVargas <arturvargas9226@gmail.com>
2025-06-10 12:10:36 +0200

    v1 send payments to evm

--- src/core/send-payment-to-starknet.ts
+++ src/core/send-payment-to-starknet.ts
@@ -1,7 +1,8 @@
 import { CrosschainNetworks, SendCrossChainParamsTyped, SendCrossChainWithAuthParams } from "./types";
 import { decryptPrivateKey } from "./lib/encryption";
 import { BACKEND_URL } from "./backend-url";
-
+import { Account, Call, RpcProvider } from "starknet";
+ 
 export interface CrosschainPaymentParams {
   apiPublicKey: string;
   bearerToken: string;
@@ -24,15 +25,16 @@ export const sendPaymentStarknetToArbitrum = async (
   try {
     const { apiPublicKey, bearerToken, encryptKey, wallet, destinationAddress, destinationAsset, amount, sourceChain, sourceAsset } = params;
 
-    /* const privateKeyDecrypted = decryptPrivateKey(
+     const privateKeyDecrypted = decryptPrivateKey(
         wallet.encryptedPrivateKey,
         encryptKey
     );
 
     if (!privateKeyDecrypted) {
         throw new Error("Failed to decrypt private key");
-    } */
+    }
 
+    // Initiate layerswap process
     const paymentRequest = await fetch(`${BACKEND_URL}/crosschain/starknet-to-evm-payment`, {
       method: "POST",
       headers: {
@@ -57,13 +59,86 @@ export const sendPaymentStarknetToArbitrum = async (
 
     const result = await paymentRequest.json();
     console.log("result", result);
-    // TODO: Implementar la lógica de ejecución de la transacción
-    
-    if (!result.transactionHash) {
-      throw new Error('La respuesta no contiene el hash de la transacción');
+
+    if (!result.depositActions || result.depositActions.length === 0) {
+        throw new Error('No se recibieron acciones de depósito');
     }
 
-    return "TEST COMPLETE";
+    const depositAction = result.depositActions[0];
+    const callData = JSON.parse(depositAction.call_data);
+    console.log("callData", callData);
+    // Finish layerswap process
+    
+    const calls: Call[] = callData.map((call: any) => ({
+        contractAddress: call.contractAddress,
+        entrypoint: call.entrypoint,
+        calldata: call.calldata,
+      }));
+
+      console.log("calls", calls);
+
+    const provider = new RpcProvider({
+        nodeUrl: "https://cloud.argent-api.com/v1/starknet/mainnet/rpc/v0.7",
+      });
+  
+      const account = new Account(
+        provider,
+        wallet.publicKey,
+        privateKeyDecrypted
+      );
+  
+      // Build the type data
+      const typeDataResponse = await fetch(`${BACKEND_URL}/transactions/prepare-typed-data`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${bearerToken}`,
+          'X-API-Key': apiPublicKey,
+        },
+        body: JSON.stringify({
+          publicKey: wallet.publicKey,
+          calls: calls,
+          accountClassHash: "0x036078334509b514626504edc9fb252328d1a240e4e948bef8d0c08dff45927f"
+        }),
+      });
+  
+      if (!typeDataResponse.ok) {
+        const errorText = await typeDataResponse.text();
+        throw new Error(`Error en la API: ${errorText}`);
+      }
+  
+      const typeData = await typeDataResponse.json();
+  
+      // Sign the message
+      const userSignature = await account.signMessage(typeData);
+      //console.log("User signature: ", userSignature);
+  
+      const executeTransaction = await fetch(`${BACKEND_URL}/transactions/execute-sponsored-transaction`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${bearerToken}`,
+          'X-API-Key': apiPublicKey,
+        },
+        body: JSON.stringify({
+          publicKey: wallet.publicKey,
+          typeData: typeData,
+          userSignature: {
+            r: (userSignature as any).r.toString(),
+            s: (userSignature as any).s.toString(),
+            recovery: (userSignature as any).recovery
+          }
+        }),
+      });
+  
+      if (!executeTransaction.ok) {
+        const errorText = await executeTransaction.text();
+        throw new Error(`Error en la API de ejecución: ${errorText}`);
+      }
+    
+      const transactionResult = await executeTransaction.json();
+      return transactionResult.transactionHash;
+
   } catch (error) {
     console.error("Error executing cross-chain transaction:", error);
     throw error;
